acdb62b7fcb5cc8482ec7b8d2b45b5c7948bfb7f
wnk <rony.lopez@lumail.net>
2019-04-29 00:57:38 -0600

    Adding values

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-container>
+    <v-container dense>
       <v-layout row wrap>
         <v-flex>
           <v-card>
@@ -35,24 +35,13 @@
                 </v-layout>
               </v-container>
             </v-form>
-            <v-layout row>
-              <v-flex sm12>
-                <v-autocomplete
-                  v-model="selected"
-                  :items="texto"
-                  item-text="name"
-                  item.value="id"
-                >
-                </v-autocomplete>
-              </v-flex>
-            </v-layout>
             <v-layout>
               <v-flex sm12>
                 <v-toolbar flat color="white">
                   <v-toolbar-title>My CRUD</v-toolbar-title>
                   <v-divider class="mx-2" inset vertical></v-divider>
                   <v-spacer></v-spacer>
-                  <v-dialog v-model="dialog" max-width="500px">
+                  <v-dialog v-model="dialog" max-width="800px">
                     <template v-slot:activator="{ on }">
                       <v-btn color="primary" dark class="mb-2" v-on="on"
                         >New Item</v-btn
@@ -64,36 +53,39 @@
                       </v-card-title>
 
                       <v-card-text>
-                        <v-container grid-list-md>
+                        <v-container grid-list-sm>
                           <v-layout wrap>
-                            <v-flex xs12 sm6 md4>
-                              <v-text-field
-                                v-model="editedItem.name"
-                                label="Dessert name"
-                              ></v-text-field>
+                            <v-flex xs12 sm6 md6>
+                              <v-autocomplete
+                                v-model="editedItem.cuenta"
+                                :items="texto"
+                                item-text="name"
+                                item.value="id"
+                              >
+                              </v-autocomplete>
                             </v-flex>
                             <v-flex xs12 sm6 md4>
                               <v-text-field
-                                v-model="editedItem.calories"
-                                label="Calories"
+                                v-model="editedItem.descripcion"
+                                label="descripcion"
                               ></v-text-field>
                             </v-flex>
-                            <v-flex xs12 sm6 md4>
+                            <v-flex xs12 sm6 md2>
                               <v-text-field
-                                v-model="editedItem.fat"
-                                label="Fat (g)"
+                                v-model="editedItem.documento"
+                                label="documento"
                               ></v-text-field>
                             </v-flex>
-                            <v-flex xs12 sm6 md4>
+                            <v-flex xs12 sm6 md6>
                               <v-text-field
-                                v-model="editedItem.carbs"
-                                label="Carbs (g)"
+                                v-model="editedItem.debe"
+                                label="debe"
                               ></v-text-field>
                             </v-flex>
-                            <v-flex xs12 sm6 md4>
+                            <v-flex xs12 sm6 md6>
                               <v-text-field
-                                v-model="editedItem.protein"
-                                label="Protein (g)"
+                                v-model="editedItem.haber"
+                                label="haber"
                               ></v-text-field>
                             </v-flex>
                           </v-layout>
@@ -118,11 +110,11 @@
                   class="elevation-1"
                 >
                   <template v-slot:items="props">
-                    <td>{{ props.item.name }}</td>
-                    <td class="text-xs-right">{{ props.item.calories }}</td>
-                    <td class="text-xs-right">{{ props.item.fat }}</td>
-                    <td class="text-xs-right">{{ props.item.carbs }}</td>
-                    <td class="text-xs-right">{{ props.item.protein }}</td>
+                    <td>{{ props.item.cuenta }}</td>
+                    <td class="text-xs-right">{{ props.item.descripcion }}</td>
+                    <td class="text-xs-right">{{ props.item.documento }}</td>
+                    <td class="text-xs-right">{{ props.item.debe }}</td>
+                    <td class="text-xs-right">{{ props.item.haber }}</td>
                     <td class="justify-center layout px-0">
                       <v-icon small class="mr-2" @click="editItem(props.item)">
                         edit
@@ -157,32 +149,32 @@ export default {
       dialog: false,
       headers: [
         {
-          text: 'Dessert (100g serving)',
+          text: 'Cuenta',
           align: 'left',
           sortable: false,
           value: 'name'
         },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
+        { text: 'Descripcion', value: 'calories' },
+        { text: 'Documento', value: 'fat' },
+        { text: 'Debe', value: 'carbs' },
+        { text: 'Haber', value: 'protein' },
         { text: 'Actions', value: 'name', sortable: false }
       ],
       desserts: [],
       editedIndex: -1,
       editedItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
+        cuenta: '',
+        descripcion: 0,
+        documento: 0,
+        debe: 0,
+        haber: 0
       },
       defaultItem: {
-        name: '',
-        calories: 0,
-        fat: 0,
-        carbs: 0,
-        protein: 0
+        cuenta: '',
+        descripcion: 0,
+        documento: 0,
+        debe: 0,
+        haber: 0
       }
     }
   },
@@ -199,7 +191,6 @@ export default {
   },
 
   created() {
-    this.initialize()
     axios.get('https://conta-1-9e105.firebaseio.com/cuentas.json').then(res => {
       for (const key in res.data) {
         this.texto.push(res.data[key])
@@ -208,81 +199,6 @@ export default {
   },
 
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7
-        }
-      ]
-    },
-
     editItem(item) {
       this.editedIndex = this.desserts.indexOf(item)
       this.editedItem = Object.assign({}, item)
